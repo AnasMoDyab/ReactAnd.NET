770d122a62a97a88c9a1875b7df7d6f84b794804
AnasMoDyab <anasdyab89@gmail.com>
2021-02-16 17:57:07 +0100

    Add interface to client-app

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
 import { List, Header } from 'semantic-ui-react';
-
+import {Activity} from '../models/activity'
 
 
 function App() {
-const [activities, setActivities]= useState([]);
+const [activities, setActivities]= useState<Activity[]>([]); // pass type of activities to usestate<Activity[]> Activity is the interface
 
 useEffect(()=> {
-  axios.get('http://localhost:5000/api/activities')
+  axios.get<Activity[]>('http://localhost:5000/api/activities')
   .then(response=> {
-    console.log(response);
+    console.log(response)
     setActivities(response.data);
   })
 
-  console.log(activities);
+
 
 
 },[]);
@@ -29,7 +29,7 @@ useEffect(()=> {
   
       />
         <List>
-          {activities.map((activity:any)=> {
+          {activities.map(activity=> {
               return <List.Item key={activity.id}>
                 {activity.title}
                 </List.Item>
