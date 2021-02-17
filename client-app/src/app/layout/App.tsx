ff2f0a424683404ca95dd4c82137e615ea061942
AnasMoDyab <anasdyab89@gmail.com>
2021-02-17 15:50:57 +0100

    configure mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import {  Container } from 'semantic-ui-react';
 import {Activity} from '../models/activity'
 import NavBar from '../layout/NavBar'
@@ -7,27 +6,22 @@ import ActivityDashboard from '../../features/activities/dachboard/ActivityDashb
 import {v4 as uuid} from 'uuid';
 import agent from '../api/agent'
 import LoadingComponent from '../layout/LoadingComponent'
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
+
+const {activityStore}= useStore();
+
 const [activities, setActivities]= useState<Activity[]>([]); // pass type of activities to usestate<Activity[]> Activity is the interface
 const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 const [editMode, setEditMode]= useState(false);
 const [openModel, setOpenModel] = React.useState(false)
-const [loading, setLoading]= useState(true);
 const [submitting, setSubmitting]= useState(false);
-useEffect(()=> {
-  agent.Activities.list()
-  .then(response=> {
-    let activities: Activity[]=[];
-    response.forEach(activity => {
-      activity.date= activity.date.split('T')[0];
-      activities.push(activity);
-    })
-    setActivities(activities);
-    setLoading(false);
-  })
 
-},[]);
+useEffect(()=> {
+  activityStore.loadActivities();
+},[activityStore]);
 
  function handleSelectActivity(id: string){
    setSelectedActivity(activities.find(x=>x.id===id));
@@ -85,7 +79,7 @@ function handleDeleteActivity(id: string){
   setActivities([...activities.filter(x=> x.id !==id)])
 }
 
-    if(loading) return <LoadingComponent content= 'Loading app' />
+    if(activityStore.loadingInitial) return <LoadingComponent content= 'Loading app' />
 
 
   return (
@@ -93,8 +87,9 @@ function handleDeleteActivity(id: string){
       <NavBar  openForm={handleFormOpen}/>
 
       <Container style={{marginTop: '7em'}}>
+      
       <ActivityDashboard 
-      activities={activities}
+      activities={activityStore.activities}
       selectedActivity={selectedActivity}
       selectActivity={handleSelectActivity}
       cancelSelectActivity= {handleCancelSelectActivity}
@@ -117,4 +112,4 @@ function handleDeleteActivity(id: string){
   );
 }
 
-export default App;
+export default observer(App);
