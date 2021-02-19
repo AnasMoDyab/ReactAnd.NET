0ec1a4f0e057c7412ee7a86ce0b6f596314912b7
AnasMoDyab <anasdyab89@gmail.com>
2021-02-19 16:57:57 +0100

    create validation error

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,12 +4,12 @@ import NavBar from '../layout/NavBar'
 import ActivityDashboard from '../../features/activities/dachboard/ActivityDashboard'
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
-
-
-
+import TestErrors from '../../features/errors/TestError'
+import {ToastContainer} from 'react-toastify'
+import NotFound from '../../features/errors/NotFound'
 
 
 function App() {
@@ -17,6 +17,7 @@ const location = useLocation();
 
 return (
   <>
+ <ToastContainer position='bottom-right' hideProgressBar />
     <Route exact path='/' component={HomePage} />
     <Route
       path={'/(.+)'}
@@ -24,9 +25,13 @@ return (
         <>
           <NavBar />
           <Container style={{ marginTop: '7em' }}>
-            <Route exact path='/activities' component={ActivityDashboard} />
-            <Route path='/activities/:id' component={ActivityDetails} />
-            <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            <Switch>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Route path="/errors" component={TestErrors} />
+              <Route component={NotFound}/>
+            </Switch>
           </Container>
         </>
       )}
---
+++ client-app/src/features/errors/NotFound.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Button, Header, Icon, Segment} from 'semantic-ui-react';
+import {Link} from 'react-router-dom'
+
+export default function  NotFound(){
+    return (
+        <Segment placeholder>
+            <Header icon>
+                <Icon name='search'/>
+                Oops- we've looked everwhere and could not find this.
+            </Header>
+            <Segment.Inline>
+                <Button  as= {Link} to='/activitiez' primary>
+                    Return to activities page
+                </Button>
+            </Segment.Inline>
+        </Segment>
+    )
+}
