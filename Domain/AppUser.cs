15980694aab3993ee6a51052916795ba548dfd8e
AnasMoDyab <anasdyab89@gmail.com>
2021-02-23 18:18:10 +0100

    Add Photo entity

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -8,5 +8,9 @@ namespace Domain
         public string DisplayName { get; set; }
         public string Bio{ get; set; }
 
-    public ICollection<ActivityAttendee> Activities { get; set; }    }
+    public ICollection<ActivityAttendee> Activities { get; set; }   
+    
+    public ICollection<Photo> Photos { get; set; } 
+    
+    }
 }
