3f19c7954b66416fefd355def7739c4a0eac3232
AnasMoDyab <anasdyab89@gmail.com>
2021-03-06 14:05:12 +0100

    adding paging to API

---
+++ API/Extensions/HttpExtension.cs
@@ -0,0 +1,24 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Extensions
+{
+    public static class HttpExtension
+    {
+        public static void AddPaginationHeader(this HttpResponse response, 
+                int currentPage, int itemPerPage, int totalItem, int totalPage )
+        {
+            var paginationHeader = new
+            {
+                currentPage,
+                itemPerPage,
+                totalItem,
+                totalPage
+            };
+
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            response.Headers.Add("Access-control-Expose-Headers", "Pagination");
+
+        }
+    }
+}
